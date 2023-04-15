d6edd487bb17bb403721ab5d654d3f57a9cedebc
Roger Toledo <rogertoledo28@gmail.com>
2023-04-15 20:06:52 -0300

    feat: adiciona salvar cookie

--- webapp/go.mod
+++ webapp/go.mod
@@ -2,4 +2,9 @@ module webapp
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/gorilla/securecookie v1.1.1 // indirect
--- webapp/src/controller/login.go
+++ webapp/src/controller/login.go
@@ -3,7 +3,10 @@ package controller
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/cookies"
 	"webapp/src/modelos"
 	"webapp/src/respostas"
 )
@@ -20,7 +23,8 @@ func FazerLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response, erro := http.Post("http://localhost:5000/login", "application/json", bytes.NewBuffer(usuario))
+	url := fmt.Sprintf("%s/login", config.ApiUrl)
+	response, erro := http.Post(url, "application/json", bytes.NewBuffer(usuario))
 	if erro != nil {
 		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
 		return
@@ -31,9 +35,14 @@ func FazerLogin(w http.ResponseWriter, r *http.Request) {
 		respostas.TratarStatusCodeErro(w, response)
 		return
 	}
-	
+
 	var dadosAutenticacao modelos.DadosAutenticacao
 	if erro := json.NewDecoder(response.Body).Decode(&dadosAutenticacao); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	if erro = cookies.Salvar(w, dadosAutenticacao.ID, dadosAutenticacao.Token); erro != nil {
 		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
 		return
 	}
