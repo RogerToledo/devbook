a57e99bf3d8622f567ce07b088ea54468eb0108c
Roger Toledo <rogertoledo28@gmail.com>
2023-05-23 22:03:05 -0300

    refactor: bloqueia que usuário logado acesse página de login

--- webapp/src/controller/paginas.go
+++ webapp/src/controller/paginas.go
@@ -11,9 +11,17 @@ import (
 	"webapp/src/requisicoes"
 	"webapp/src/respostas"
 	"webapp/src/utils"
+
+	"github.com/gorilla/mux"
 )
 
 func CarregarTelaLogin(w http.ResponseWriter, r *http.Request) {
+	cookies, _ := cookies.Ler(r)
+	if cookies["token"] != "" {
+		http.Redirect(w, r, "/home", 302)
+		return
+	}
+
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
@@ -48,4 +56,33 @@ func CarregaPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 		Publicacoes: publicacoes,
 		UsuarioID:   usuarioID,
 	})
+}
+
+func CarregaPaginaEdicao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoID"], 10, 64)
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("%s/publicacoes/%d", config.ApiUrl, publicacaoID)
+	response, erro := requisicoes.FazerRequisicaoAutenticacao(r, http.MethodGet, url, nil)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeErro(w, response)
+	}
+
+	var publicacao modelos.Publicacoes
+	if erro = json.NewDecoder(response.Body).Decode(&publicacao); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	utils.ExecutarTemplate(w, "atualizar-publicacao.html", publicacao)
 }
