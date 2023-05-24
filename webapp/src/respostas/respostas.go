251787c382fa6986ba815c3be72f3fb2b217b524
Roger Toledo <rogertoledo28@gmail.com>
2023-05-23 22:04:46 -0300

    refactor: corrige resposta quando statusCode for igual a StatusNoContent

--- webapp/src/respostas/respostas.go
+++ webapp/src/respostas/respostas.go
@@ -10,15 +10,17 @@ type ErroAPI struct {
 	Erro string `json:"erro"`
 }
 
-func JSON(w http.ResponseWriter, statusCode int, dados interface{}){
+func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
-	if dados != nil {
+	if statusCode != http.StatusNoContent{
 		if erro := json.NewEncoder(w).Encode(dados); erro != nil {
 			log.Fatal(erro)
 		}
 	}
+	
+
 }
 
 func TratarStatusCodeErro(w http.ResponseWriter, r *http.Response) {
